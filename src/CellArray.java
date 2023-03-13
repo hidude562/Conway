e9339addb585792ba2a24fff4e95f75db5532764
Nathan M <82677882+hidude562@users.noreply.github.com>
2023-03-12 20:10:13 -0400

    fix some bugs

--- src/CellArray.java
+++ src/CellArray.java
@@ -2,19 +2,30 @@ public class CellArray {
     private Cell[][] cells;
     private static final int sizeX = 50;
     private static final int sizeY = 50;
+    private int iter;
 
     // Blocks, bee-hive, loaf, boat, tub, blinker, Toad, beacon, glider, small, med, large spaceship
     protected static int[] countOfEachType;
 
+
     public CellArray() {
-        cells = new Cell[50][50];
+        cells = new Cell[sizeX][sizeY];
+        iter = 0;
+
+        // Initialize each member of array
+        for(int x0 = 0; x0 < sizeX; x0++) {
+            for(int y0 = 0; y0 < sizeY; y0++) {
+                cells[x0][y0] = new Cell();
+            }
+        }
     }
     public void nextStep() {
+        iter++;
         Cell[][] tempCells = cells.clone();
         for(int x0 = 0; x0 < sizeX; x0++) {
-            for(int y0 = 0; y0 < sizeX; y0++) {
+            for(int y0 = 0; y0 < sizeY; y0++) {
                 tempCells[x0][y0].incrementTime();
-                int cellSurroundings = getSurroundings(x0, y0, tempCells);
+                int cellSurroundings = getSurroundings(x0, y0);
                 if(cellSurroundings < 2 || cellSurroundings > 3)
                     tempCells[x0][y0].setDead();
                 else if(cellSurroundings == 3)
@@ -23,13 +34,29 @@ public class CellArray {
         }
         cells = tempCells.clone();
     }
-    public int getSurroundings(int x0, int y0, Cell[][] cells) {
-        int liveCells = 0;
+    public int getSurroundings(int x0, int y0) {
+        int liveCells = (cells[x0][y0].get() ? -1 : 0);
         for(int x1 = x0 - 1; x1 <= x0 + 1; x1++) {
             for(int y1 = y0 - 1; y1 <= y0 + 1; y1++) {
                 liveCells += (cells[Math.floorMod(x1, sizeX)][Math.floorMod(y1, sizeY)].get() ? 1 : 0);
             }
         }
         return liveCells;
+    }
+    public void print() {
+        System.out.print("\n\n");
+        System.out.print("Iter: " + iter + " ");
+
+
+        System.out.print("\n");
+        for(int x0 = 0; x0 < sizeX; x0++) {
+            for(int y0 = 0; y0 < sizeY; y0++) {
+                if(cells[x0][y0].get())
+                    System.out.print("@");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
     }
 }
